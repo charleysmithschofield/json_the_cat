88fc7312f37544d4e6efa88f7804ae2be7ffa6e3
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-24 18:13:08 -0700

    remove steps and move to README.md

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,4 @@
 
-/*
-Implmenting in Node:
-
-Write the logic to fetch the Siberian data from the API endpoint
-using request
-*/
-
-
-/*
-Steps:
-
-1. Requite the request library.
-2. Define the API endpoint URL.
-3. Get the breed name from command-line arguments.
-4. Use the `request` library to make a GET request to the API endpoint.
-5. If there is an error or if the response statusCode is not 200 (200 = everything's ok)
-  5a. Print the error
-  5b. If no error, log the body and the typeof body (should be a string type).
-6. Use JSON.parse to convert the JSON string into an object.
-7. Check if data array is empty, return 'Breed not found.'
-  7a. If breed is found, access the first entry in the data array and print out the description for the user.
-*/
 
 // Require the request library
 const request = require('request');
